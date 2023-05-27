26f95d7bfb700c021b942f6d144fa0a23faf004b
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-05-28 00:12:15 +0500

    added key properly

--- src/components/AvailableBookList.js
+++ src/components/AvailableBookList.js
@@ -5,7 +5,7 @@ const AvailableBookList = (props) => {
       <div className="book__list_block-container">
         {props.bookList.map((el, index) => {
           return (
-            <div className="book__list-item">{`${index + 1}. "${
+            <div key={index} className="book__list-item">{`${index + 1}. "${
               el.bookName
             }"`}</div>
           );
