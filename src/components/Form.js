278b1dd1b6830ea731e99e37726efca241f70851
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-05-28 00:09:42 +0500

    minor alert change, made input clean after submitting book

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,22 +14,25 @@ const Form = (props) => {
   const onSubmitBook = async () => {
     try {
       if (!props.bookName) {
-        alert("Книга должна иметь название");
+        alert("Книга должна иметь название.");
       } else if (props.bookName.length > 100) {
-        alert("Название книги не может превышать 100 символов");
+        alert("Название книги не может превышать 100 символов.");
       } else if (!props.authorList) {
-        alert("Должен быть хотя бы один автор");
+        alert("Должен быть хотя бы один автор.");
       } else if (props.publicationYear < 1800) {
-        alert("Год публикации должен быть не раньше 1800 года");
+        alert("Год публикации должен быть не раньше 1800 года.");
+      } else if (props.publicationYear > new Date().getFullYear()) {
+        alert("Год публикации должен быть не позже текущего года.");
       } else if (
         props.bookRating >= 10 ||
         props.bookRating.includes(".") ||
         props.bookRating.includes(",")
       ) {
-        alert("Рейтинг должен быть целым числом от 0 до 10");
+        alert("Рейтинг должен быть целым числом от 0 до 10.");
       } else {
         await addDoc(props.booksCollectionRef, props.data);
         props.getBookList();
+        props.setData(props.initialState);
       }
     } catch (err) {
       console.error(err);
@@ -70,7 +73,7 @@ const Form = (props) => {
         name="ISBN"
       />
 
-      <button onClick={onSubmitBook}> Submit book</button>
+      <button onClick={onSubmitBook}> Добавить книгу</button>
     </form>
   );
 };
